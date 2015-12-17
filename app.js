35aa3b9d1cc0e153010e5cc766b7407222cf7622
nxq90 <arsenal.nxq@gmail.com>
2015-12-17 17:39:24 +0700

    integrate electrolyte

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var app     = express();
+var container = require('electrolyte');
+var express   = require('express');
+var app       = express();
 
+container.use(container.node_modules());
+container.use(container.node('src'));
 //TODO
 
 module.exports = app;
